c155afba0d2d97cfe3c716861e0ee65a29a37f97
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2023-07-02 23:30:08 +0700

    add body page

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
@@ -51,7 +52,8 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 ],
               )
             )
-          )
+          ),
+          const FoodPageBody(),
         ]
       )
     );
