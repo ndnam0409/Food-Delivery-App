045a5602c5216f11a61b88e5cccbeccb36c04fa7
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2023-07-02 22:26:16 +0700

    first commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 45, bottom: 15),
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column( 
+                    children: [
+                      BigText(text: "Vietnam", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: "Kon Tum", color: Colors.black54),
+                          const Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ), 
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            )
+          )
+        ]
+      )
+    );
+  }
+}
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlowText;
+  BigText({Key ? key, 
+    this.color = const Color(0xFF332d2b),
+    required this.text, 
+    this.size = 20,
+    this.overFlowText = TextOverflow.ellipsis
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overFlowText,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Roboto'
+      )
+    );
+  }
+}
