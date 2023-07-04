4b2ef8ef45c3c8d5c77d170e6ab4a52d553aa5f1
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2023-07-04 10:15:43 +0700

    scale body page

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
 import '../utils/colors.dart';
@@ -13,6 +14,27 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  // ignore: unused_field
+  var _currentPageValue = 0.0;
+  final _scaleFactor = 0.8;
+  double _height = 220;
+
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+        
+      });
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -27,203 +49,121 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             itemBuilder: (context, position){
             return _buildPageItem(position);
           }),
-        ),
-        Container(
-          margin: const EdgeInsets.only(left: 25),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.circle_notifications,
-                    color: AppColors.mainColor,
-                  ),
-                  const SizedBox(width: 10),
-                  BigText(text: "Popular", size: 18),
-                  const SizedBox(width: 15),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Row(
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              height: 150,
-                              width:150,
-                              margin: const EdgeInsets.only(left: 10, right: 10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.red,
-                                image: const DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                    "assets/image/food2.jpg"
-                                  )
-                               )
-                              ),
-                            ),
-                            Container(
-                              height: 150,
-                              width:180,
-                              margin: const EdgeInsets.only(left: 10, right: 10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                child: Row(
-                                    children: [
-                                      const Icon(Icons.restaurant, size: 16),
-                                      const SizedBox(width: 10),
-                                      SmallText(text: "Grilled Lamb",size: 14)
-                                    ],
-                                  )
-                              )
-                            ),
-                          ],
-                        ),
-                        
-                        Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 150,
-                                width:150,
-                                margin: const EdgeInsets.only(left: 10, right: 10),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Colors.red,
-                                  image: const DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                      "assets/image/food3.jpg"
-                                    )
-                                 )
-                                ),
-                              ), 
-                              Container(
-                                height: 150,
-                                width:170,
-                                margin: const EdgeInsets.only(left: 10, right: 10),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Colors.white,
-                                ),
-                                child: Container(
-                                child: Row(
-                                    children: [
-                                      const Icon(Icons.restaurant, size: 16),
-                                      const SizedBox(width: 10),
-                                      SmallText(text: "Italian Salad",size: 14),
-                                      const SizedBox(width: 10),
-                                    ],
-                                  )
-                              )
-                              ),
-                              
-                            ]
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
         )
       ],
     );
   }
   Widget _buildPageItem(int index){
-    return Stack(
-      children: [
-          Container(
-            height: 220,
-            margin: const EdgeInsets.only(left: 10, right: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(
-                  "assets/image/food1.jpg"
-                )
-              )
-            )
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 120,
-              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currentPageValue.floor()){
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    } else if (index == _currentPageValue.floor() + 1){
+      var currentScale = _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currTrans, 0);
+      
+    } else if (index == _currentPageValue.floor() - 1){
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    } else{
+      var currentScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, _height*(1 - _scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+            Container(
+              height: 220,
+              margin: const EdgeInsets.only(left: 10, right: 10),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: Colors.white,
-              ),
-              child: Container(
-                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    BigText(text: "Roasted Chicken"),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Wrap(
-                          children: List.generate(5, (index) => Icon(
-                            Icons.star, 
-                            size: 15,
-                            color: AppColors.mainColor,
-                          )),
-                        ),
-                        const SizedBox(width: 15),
-                        SmallText(text: "5.0"),
-                        const SizedBox(width: 15),
-                        SmallText(text: "1234 comments")
-                      ],
-                    ), 
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.circle, 
-                            color: Colors.orangeAccent, 
-                            size: 12,
-                          ),
-                          const SizedBox(width: 8),
-                          SmallText(text: "Excellent"),
-                          const SizedBox(width: 25),
-                          Icon(
-                            Icons.location_on, 
-                            color: AppColors.mainColor, 
-                            size: 12,
-                          ),
-                          const SizedBox(width: 8),
-                          SmallText(text: "12km"),
-                          const SizedBox(width: 25),
-                          const Icon(
-                            Icons.timer,
-                            color: Color.fromARGB(255, 232, 126, 126),
-                            size: 12
-                          ),
-                          const SizedBox(width: 8),
-                          SmallText(text: "32min"),
-                        ]
-                      ),
-                    )
-                  ],
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
+                image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    "assets/image/food1.jpg"
+                  )
                 )
               )
             ),
-          ),
-      ]
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 120,
+                margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0xFFe8e8e8),
+                      blurRadius: 5.0,
+                      offset: Offset(0,5),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5,0),
+                    ),
+                  ]
+                ),
+                child: Container(
+                  padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BigText(text: "Roasted Chicken"),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(5, (index) => Icon(
+                              Icons.star, 
+                              size: 15,
+                              color: AppColors.mainColor,
+                            )),
+                          ),
+                          const SizedBox(width: 15),
+                          SmallText(text: "5.0"),
+                          const SizedBox(width: 15),
+                          SmallText(text: "1234 comments")
+                        ],
+                      ),
+                      const SizedBox(height: 15), 
+                      Row(
+                        children: [
+                          IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Excellent",  
+                            iconColor: AppColors.iconColor1,
+                          ),
+                          const SizedBox(width: 32),
+                          IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "2.4km",  
+                            iconColor: AppColors.mainColor,
+                          ),
+                          const SizedBox(width: 32),
+                          IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: "15min",  
+                            iconColor: AppColors.iconColor2,
+                          )
+                        ]
+                      )
+                    ],
+                  )
+                )
+              ),
+            ),
+        ]
+      ),
     );
   }
 }
